de7eaba744c0455db058f32e20742c5108e82b8c
Elliot Hanson <hans7680@morris.umn.edu>
2026-08-28 19:10:21 -0500

    Update game theory page

--- game-theory.js
+++ game-theory.js
@@ -929,21 +929,29 @@ class Game {
             }
 
             // apply that equilibrium to the original matrices
-            const rowDisagree = equilibrium[0]*equilibrium[1]*this.row_matrix[0] + equilibrium[0]*(1-equilibrium[1])*this.row_matrix[1]
-                            + (1-equilibrium[0])*equilibrium[1]*this.row_matrix[2] + (1-equilibrium[0])*(1-equilibrium[1])*this.row_matrix[3];
-            const colDisagree = equilibrium[0]*equilibrium[1]*this.col_matrix[0] + equilibrium[0]*(1-equilibrium[1])*this.col_matrix[1]
-                            + (1-equilibrium[0])*equilibrium[1]*this.col_matrix[2] + (1-equilibrium[0])*(1-equilibrium[1])*this.col_matrix[3];
-            this.#threat_point = [rowDisagree,colDisagree];
+            // const rowDisagree = equilibrium[0]*equilibrium[1]*this.row_matrix[0] + equilibrium[0]*(1-equilibrium[1])*this.row_matrix[1]
+            //                 + (1-equilibrium[0])*equilibrium[1]*this.row_matrix[2] + (1-equilibrium[0])*(1-equilibrium[1])*this.row_matrix[3];
+            // const colDisagree = equilibrium[0]*equilibrium[1]*this.col_matrix[0] + equilibrium[0]*(1-equilibrium[1])*this.col_matrix[1]
+            //                 + (1-equilibrium[0])*equilibrium[1]*this.col_matrix[2] + (1-equilibrium[0])*(1-equilibrium[1])*this.col_matrix[3];
+            const rowDisagree = equilibrium[0]*equilibrium[1]*A[0] + equilibrium[0]*(1-equilibrium[1])*A[1]
+                            + (1-equilibrium[0])*equilibrium[1]*A[2] + (1-equilibrium[0])*(1-equilibrium[1])*A[3];
+            const colDisagree = equilibrium[0]*equilibrium[1]*B[0] + equilibrium[0]*(1-equilibrium[1])*B[1]
+                            + (1-equilibrium[0])*equilibrium[1]*B[2] + (1-equilibrium[0])*(1-equilibrium[1])*B[3];
+            this.#threat_point = [rowDisagree/2,colDisagree/2];
 
             // if there are two equally valid threat points, compute the second one
             if (!mixed && (A[3] <= A[1] || A[2] <= A[0]) && (A[1] <= A[3] || A[0] <= A[2]) && (B[3] <= B[2] || B[1] <= B[0]) && (B[2] <= B[3] || B[0] <= B[1])) {
                 let equilibrium2 = [(B[3] - B[2])/(B[0] - B[1] - B[2] + B[3]), (A[3] - A[1])/(A[0] - A[1] - A[2] + A[3])];
-                const rowDisagree2 = equilibrium2[0]*equilibrium2[1]*this.row_matrix[0] + equilibrium2[0]*(1-equilibrium2[1])*this.row_matrix[1]
-                                + (1-equilibrium2[0])*equilibrium2[1]*this.row_matrix[2] + (1-equilibrium2[0])*(1-equilibrium2[1])*this.row_matrix[3];
-                const colDisagree2 = equilibrium2[0]*equilibrium2[1]*this.col_matrix[0] + equilibrium2[0]*(1-equilibrium2[1])*this.col_matrix[1]
-                                + (1-equilibrium2[0])*equilibrium2[1]*this.col_matrix[2] + (1-equilibrium2[0])*(1-equilibrium2[1])*this.col_matrix[3];
+                // const rowDisagree2 = equilibrium2[0]*equilibrium2[1]*this.row_matrix[0] + equilibrium2[0]*(1-equilibrium2[1])*this.row_matrix[1]
+                //                 + (1-equilibrium2[0])*equilibrium2[1]*this.row_matrix[2] + (1-equilibrium2[0])*(1-equilibrium2[1])*this.row_matrix[3];
+                // const colDisagree2 = equilibrium2[0]*equilibrium2[1]*this.col_matrix[0] + equilibrium2[0]*(1-equilibrium2[1])*this.col_matrix[1]
+                //                 + (1-equilibrium2[0])*equilibrium2[1]*this.col_matrix[2] + (1-equilibrium2[0])*(1-equilibrium2[1])*this.col_matrix[3];
+                const rowDisagree2 = equilibrium2[0]*equilibrium2[1]*A[0] + equilibrium2[0]*(1-equilibrium2[1])*A[1]
+                                + (1-equilibrium2[0])*equilibrium2[1]*A[2] + (1-equilibrium2[0])*(1-equilibrium2[1])*A[3];
+                const colDisagree2 = equilibrium2[0]*equilibrium2[1]*B[0] + equilibrium2[0]*(1-equilibrium2[1])*B[1]
+                                + (1-equilibrium2[0])*equilibrium2[1]*B[2] + (1-equilibrium2[0])*(1-equilibrium2[1])*B[3];
                 if (Math.abs(this.#threat_point[0]-rowDisagree2) > 0.001 && Math.abs(this.#threat_point[1]-colDisagree2) > 0.001)
-                    this.#threat_point_2 = [rowDisagree2,colDisagree2];
+                    this.#threat_point_2 = [rowDisagree2/2,colDisagree2/2];
             }
         }
         return this.#threat_point;
@@ -6040,10 +6048,10 @@ function colorFunction(value,vMode) {
     //                 [236,174,94],[242,190,113],[249,206,133],[255,222,153],[255,242,191],[142,255,142],[0,113,0],[255,255,255]];
     // const cutoffs = [0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.25,1.5,2];
     if (vMode == 8) {
-        colors = [[0,0,0],[0,0,86],[111,118,172],[255,255,255],[184,114,116],[86,0,0],[0,0,0]];
-        cutoffs = [0,0.25,0.375,0.5,0.625,0.75,1];
+        colors = [[0,0,65],[0,0,86],[111,118,172],[255,255,255],[184,114,116],[86,0,0],[65,0,0]];
+        cutoffs = [0,0.2,0.375,0.5,0.625,0.8,1];
     } else if (vMode == 7) {
-        colors = [[0,0,10],[0,0,86],[111,118,172],[255,255,255],[184,114,116],[86,0,0],[10,0,0]];
+        colors = [[0,0,65],[0,0,86],[111,118,172],[255,255,255],[184,114,116],[86,0,0],[65,0,0]];
         cutoffs = [0,0.1,0.3,0.5,0.7,0.9,1];
     } else {
         colors = [[0,7,105],[0,98,162],[48,175,149],[94,190,64],[195,167,48],[199,111,8],[198,34,41],[243,178,188],[255,222,226]];
